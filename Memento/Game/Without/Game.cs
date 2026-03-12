4d637f138552cd9a32a365c915fc48704cabea36
iAhmadMahmoud <ahmee7119@gmail.com>
2026-03-13 00:37:18 +0200

    implement the game without Memento

---
+++ Memento/Game/Without/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Game.Without
+{
+    class Game
+    {
+        public int Level;
+        public int Health;
+        public string Position;
+
+        private List<(int, int, string)> saves = new List<(int, int, string)>();
+
+        public void Save()
+        {
+            saves.Add((Level, Health, Position));
+        }
+
+        public void Load(int index)
+        {
+            var save = saves[index];
+            Level = save.Item1;
+            Health = save.Item2;
+            Position = save.Item3;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Level: {Level}, Health: {Health}, Position: {Position}");
+        }
+    }
+}
--- Memento/Program.cs
+++ Memento/Program.cs
@@ -10,6 +10,7 @@ namespace Memento
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
