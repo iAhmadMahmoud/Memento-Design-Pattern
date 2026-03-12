76b069f742e0cb12cace0c645fafdf930e7711d9
iAhmadMahmoud <ahmee7119@gmail.com>
2026-03-13 01:01:00 +0200

    update calling the TextEditor in the main function

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Memento.Game.With;
-using Memento.Game.Without;
+
 
 
 namespace Memento
@@ -16,8 +15,43 @@ namespace Memento
 
 
 
+            #region TextEditor without Memento
+            TextEditor.Without.TextEditor editorWithoutMemento = new TextEditor.Without.TextEditor();
 
+            editorWithoutMemento.Write("Hello");
+            editorWithoutMemento.Write("Hello World");
+            editorWithoutMemento.Write("Hello World!!!");
 
+            Console.WriteLine(editorWithoutMemento.Content);
+
+            editorWithoutMemento.Undo();
+            Console.WriteLine(editorWithoutMemento.Content);
+
+            editorWithoutMemento.Undo();
+            Console.WriteLine(editorWithoutMemento.Content);
+            #endregion
+
+            #region TextEditor with Memento 
+            TextEditor.With.TextEditor editorWithMemento = new TextEditor.With.TextEditor();
+            TextEditor.With.History history = new TextEditor.With.History();
+
+            editorWithMemento.Content = "Hello";
+            history.Save(editorWithMemento.Save());
+
+            editorWithMemento.Content = "Hello World";
+            history.Save(editorWithMemento.Save());
+
+            editorWithMemento.Content = "Hello World!!!";
+
+            Console.WriteLine("Current: " + editorWithMemento.Content);
+
+            // Undo
+            editorWithMemento.Restore(history.Undo());
+            Console.WriteLine("After Undo: " + editorWithMemento.Content);
+
+            editorWithMemento.Restore(history.Undo());
+            Console.WriteLine("After Undo Again: " + editorWithMemento.Content);
+            #endregion
 
 
             #region Game without Memento
@@ -43,7 +77,7 @@ namespace Memento
 
             #region Game with Memento
             Game.With.Game gameWithMemento = new Game.With.Game();
-            SaveManager manager = new SaveManager();
+            Game.With.SaveManager manager = new Game.With.SaveManager();
 
             gameWithMemento.Level = 1;
             gameWithMemento.Health = 100;
