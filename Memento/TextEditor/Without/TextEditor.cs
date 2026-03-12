9a76cf1360dc829fedf8acd567cd7bb48fa659b3
iAhmadMahmoud <ahmee7119@gmail.com>
2026-03-13 00:54:04 +0200

    implement the TextEditor without Memento

---
+++ Memento/TextEditor/Without/TextEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.TextEditor.Without
+{
+    class TextEditor
+    {
+        public string Content = "";
+        private Stack<string> history = new Stack<string>();
+
+        public void Write(string newText)
+        {
+            history.Push(Content); // save old state
+            Content = newText;
+        }
+
+        public void Undo()
+        {
+            if (history.Count > 0)
+                Content = history.Pop();
+        }
+    }
+}
